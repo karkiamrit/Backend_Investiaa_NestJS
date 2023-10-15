d529af793cd3ee1f3a7168af7e9b2e83c9dc5b3b
karkiamrit <amrit.karki2073@gmail.com>
2023-10-15 20:13:17 +0545

    repo change of user for testing

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -3,4 +3,8 @@ import { Repository } from 'typeorm/repository/Repository';
 import { User } from './entities/user.entity';
 
 @CustomRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+    async getByPhone(phone: string) {
+        return this.findOne({ where: { phone } });
+    }
+}
