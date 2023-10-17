4dbfc8b8148c4561a7ea7a35282251611dede155
karkiamrit <amrit.karki2073@gmail.com>
2023-10-17 21:21:34 +0545

    maile entity setup

---
+++ src/mail/mail.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmExModule } from '../modules/decorators/typeorm.module';
+import { MailService } from './mail.service';
+import { MailRepository } from './mail.repository';
+import { MailResolver } from './mail.resolver';
+
+@Module({
+  imports: [TypeOrmExModule.forCustomRepository([MailRepository])],
+  providers: [MailService, MailResolver],
+  exports: [MailService],
+})
+export class MailModule {}
---
+++ src/mail/mail.resolver.ts
@@ -0,0 +1,32 @@
+import { GraphqlPassportAuthGuard } from '../modules/guards/graphql-passport-auth.guard';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { MailService } from './mail.service';
+import { GetManyInput, GetOneInput } from 'src/declare/inputs/custom.input';
+import { CurrentUser } from 'src/modules/decorators/query.decorator';
+import { GetMailType, Mail } from './entities/mail.entity';
+import { CreateMailInput, UpdateMailInput } from './inputs/mail.input';
+@Resolver()
+export class MailResolver {
+  constructor(private readonly mailService: MailService) {}
+
+  @Query(() => Mail)
+  @UseGuards(new GraphqlPassportAuthGuard('admin'))
+  @Mutation(() => Mail)
+  @UseGuards(new GraphqlPassportAuthGuard('admin'))
+  createMail(@Args('input') input: CreateMailInput) {
+    return this.mailService.create(input);
+  }
+
+  @Mutation(() => Mail)
+  @UseGuards(new GraphqlPassportAuthGuard('admin'))
+  updateMail(@Args('id') id: number, @Args('input') input: UpdateMailInput) {
+    return this.mailService.update(id, input);
+  }
+
+  @Mutation(() => Mail)
+  @UseGuards(new GraphqlPassportAuthGuard('admin'))
+  deleteMail(@Args('id') id: number) {
+    return this.mailService.delete(id);
+  }
+}
