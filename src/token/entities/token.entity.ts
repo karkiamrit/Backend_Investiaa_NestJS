bcf9045530f4652e30bbd4fbaf44515a35aad697
karkiamrit <amrit.karki2073@gmail.com>
2023-10-17 21:22:33 +0545

    token setup and configuration

---
+++ src/token/entities/token.entity.ts
@@ -0,0 +1,33 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { User } from 'src/user/entities/user.entity';
+
+@ObjectType()
+@Entity()
+export class Token extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Field()
+  @ManyToOne(() => User, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'user_id' })
+  user: User
+
+  @Field()
+  @Column()
+  expires_in: Date
+
+  @Field()
+  @Column({ default: false })
+  is_revoked: boolean
+
+}
+
---
+++ src/token/token.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmExModule } from '../modules/decorators/typeorm.module';
+import { TokenService } from './token.service';
+import { TokenRepository } from './token.repository';
+import { UserRepository } from 'src/user/user.repository';
+
+@Module({
+  imports: [TypeOrmExModule.forCustomRepository([TokenRepository])],
+  providers: [TokenService, TokenRepository, UserRepository],
+  exports: [TokenService],
+})
+export class TokenModule { }
