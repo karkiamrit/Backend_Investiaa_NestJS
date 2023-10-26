c33f5f95429d7c328a0fafdc62dc6baa90fba48b
karkiamrit <amrit.karki2073@gmail.com>
2023-10-26 20:45:37 +0545

    project_details entity initials

--- src/project_details/entities/project_details.entity.ts
+++ src/project_details/entities/project_details.entity.ts
@@ -11,6 +11,77 @@ export class Project_details {
   @Field(() => String)
   @Column()
   name: string;
+
+  @Field(() => String)
+  @Column()
+  status: string;
+
+  @Field(() => String)
+  @Column()
+  description: string;
+
+  @Field(() => String)
+  @Column()
+  domain: string;
+
+  @Field(() => String)
+  @Column()
+  fund_preference: string;
+
+  @Field(() => String)
+  @Column()
+  tagline: string;
+
+  @Field(() => String)
+  @Column()
+  tax_clearence_docs: string;
+
+  @Field(() => String)
+  @Column()
+  registeration_docs: string;
+
+  @Field(() => String)
+  @Column()
+  financial_projection_docs: string;
+
+  @Field(() => String)
+  @Column()
+  pitch_deck: string;
+
+  @Field(() => String)
+  @Column()
+  adhoc_file: string;
+
+  @Field(() => String)
+  @Column()
+  usp: string;
+
+  @Field(() => String)
+  @Column()
+  facebook: string;
+
+  @Field(() => String)
+  @Column()
+  linkedin: string;
+
+  @Field(() => String)
+  @Column()
+  instagram: string;
+
+  @Field(() => String)
+  @Column()
+  twitter: string;
+
+  @Field(() => String)
+  @Column()
+  team_members: string;
+
+  @Field(() => String)
+  @Column()
+  prior_investors: string;
+
+
+
 }
 
 @ObjectType()
