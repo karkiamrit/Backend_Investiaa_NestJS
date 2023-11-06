8e1867d820cce614645053249d57323fd59cf3eb
karkiamrit <amrit.karki2073@gmail.com>
2023-11-06 19:55:20 +0545

    builded custom query

--- src/project/project.repository.ts
+++ src/project/project.repository.ts
@@ -3,4 +3,11 @@ import { CustomRepository } from '../modules/decorators/typeorm.decorator';
 import { Repository } from 'typeorm/repository/Repository';
 
 @CustomRepository(Project)
-export class ProjectRepository extends Repository<Project> {}
+export class ProjectRepository extends Repository<Project> {
+  async findProjectsByEntrepreneurId(entrepreneurId: number) {
+    const projects = await this.createQueryBuilder('project')
+      .where('project.entrepreneur.id = :entrepreneurId', { entrepreneurId })
+      .getMany();
+    return projects;
+  }
+}
