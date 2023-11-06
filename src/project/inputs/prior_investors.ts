b801e9d5b30fa027a3cd0f74bfeabede81f24705
karkiamrit <amrit.karki2073@gmail.com>
2023-11-06 19:55:57 +0545

    added  missing prior investor types

--- src/project/inputs/prior_investors.ts
+++ src/project/inputs/prior_investors.ts
@@ -1,28 +1,28 @@
 import { IsOptional } from 'class-validator';
-import { Field, ID, InputType } from '@nestjs/graphql';
-import { PrimaryGeneratedColumn } from 'typeorm';
-import { YearScalar } from 'src/modules/types/scalars/year.scalar';
-// import { GraphQLUpload, Upload } from 'graphql-upload-ts';
+import { Field, ID, InputType, ObjectType } from '@nestjs/graphql';
+import { BaseEntity, PrimaryGeneratedColumn } from 'typeorm';
+import { YearScalar } from '../../modules/types/scalars/year.scalar';
 
-@InputType()
-export class TeamMemberInput {
-  @PrimaryGeneratedColumn('increment')
-  @Field(() => ID)
-  id: number;
+@ObjectType()
+@InputType('PriorInvestorInput')
+export class PriorInvestor extends BaseEntity {
+  // @PrimaryGeneratedColumn('increment')
+  // @Field(() => ID)
+  // id: number;
 
   @Field(() => String, { nullable: true })
   @IsOptional()
-  name: string;
+  name?: string;
 
-  @Field(() => YearScalar, { nullable: true })
+  @Field(() => Date, { nullable: true })
   @IsOptional()
-  date: Date;
-
-  @Field(() => Number, { nullable: true })
-  @IsOptional()
-  equity: number;
+  date?: Date;
 
   @Field(() => Number, { nullable: true })
   @IsOptional()
-  amount: number;
+  equity?: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  amount?: number;
 }
