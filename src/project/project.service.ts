98643a931b3f142737e5e7659aed489a0fa0ea9a
karkiamrit <amrit.karki2073@gmail.com>
2023-11-06 19:55:40 +0545

    added new logic after relation changes

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -5,6 +5,8 @@ import { Project } from './entities/project.entity';
 import { CreateProjectInput, UpdateProjectInput } from './inputs/project.input';
 import { User } from 'src/user/entities/user.entity';
 import { EntrepreneurService } from 'src/entrepreneur/entrepreneur.service';
+import { TeamMember } from './inputs/team_members.input';
+import { PriorInvestor } from './inputs/prior_investors';
 
 @Injectable()
 export class ProjectService {
@@ -21,12 +23,35 @@ export class ProjectService {
     return this.projectdetailsRepository.getOne(qs, query);
   }
 
-  async getProjectProfile(user: User) {
+  async getProjectProfiles(user: User) {
     const entrepreneur =
       await this.entrepreneurService.findEntrepreneurByUserId(user.id);
-    return this.projectdetailsRepository.findOne({
-      where: { entrepreneur: { id: entrepreneur.id } },
-    });
+    console.log(entrepreneur.id);
+    return this.projectdetailsRepository.findProjectsByEntrepreneurId(
+      entrepreneur.id,
+    );
+  }
+
+  private convertTeamMember(teamMembers: TeamMember[]): TeamMember[] {
+    let team_members = [];
+    for (const teamMember of teamMembers) {
+      const newTeamMember = new TeamMember();
+      Object.assign(newTeamMember, teamMember);
+      team_members.push(newTeamMember);
+    }
+    return team_members;
+  }
+
+  private convertPriorInvestor(
+    priorInvestors: PriorInvestor[],
+  ): PriorInvestor[] {
+    let prior_investors = [];
+    for (const priorInvestor of priorInvestors) {
+      const newPriorInvestor = new PriorInvestor();
+      Object.assign(newPriorInvestor, priorInvestor);
+      prior_investors.push(newPriorInvestor);
+    }
+    return prior_investors;
   }
 
   async create(input: CreateProjectInput, CurrentUser: User): Promise<Project> {
@@ -40,12 +65,22 @@ export class ProjectService {
       Object.assign(projectdetails, input);
       const currentEntrepreneur =
         await this.entrepreneurService.findEntrepreneurByUserId(CurrentUser.id);
-
+      let team_members = [];
+      let prior_investors = [];
+      if (input.team_members) {
+        team_members = this.convertTeamMember(input.team_members);
+      }
+      if (input.prior_investors) {
+        prior_investors = this.convertPriorInvestor(input.prior_investors);
+      }
       if (!currentEntrepreneur) {
         throw new Error(
           `Please register as an entrepreneur before begining your startup registeration`,
         );
       } else {
+        projectdetails.team_members = team_members;
+        projectdetails.prior_investors = prior_investors;
+
         projectdetails.entrepreneur = currentEntrepreneur;
         return await this.projectdetailsRepository.save(projectdetails);
       }
