cde678181a209dcbc46c960a723125ca84a0da4b
karkiamrit <amrit.karki2073@gmail.com>
2023-11-07 19:55:30 +0545

    added investor entity

---
+++ src/investor/entities/investor.entity.ts
@@ -0,0 +1,73 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { User } from '../../user/entities/user.entity';
+import { Profession } from '../../modules/types/common/profession.input';
+import { InvestmentRange } from '../inputs/investment_range.types';
+import { PriorInvestment } from '../inputs/prior_investment.type';
+
+@ObjectType()
+@Entity()
+export class Investor extends BaseEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Field(() => Profession)
+  @Column('json', { nullable: true })
+  profession: Profession;
+
+  @Field(() => Number, { nullable: true })
+  @Column({ nullable: true })
+  profession_experience?: number;
+
+  @Field(() => User, { nullable: true })
+  @OneToOne(() => User, { onDelete: 'CASCADE', eager: true })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Field(() => InvestmentRange, { nullable: true })
+  @Column('json', { nullable: true })
+  investment_range: InvestmentRange;
+
+  @Field(() => [PriorInvestment])
+  @Column('json', { nullable: true })
+  prior_investments: PriorInvestment[];
+
+  @Field(() => [String])
+  @Column('simple-array', { nullable: true })
+  preferred_investment_stage: string[];
+
+  @Field(() => [String])
+  @Column('simple-array', { nullable: true })
+  investment_preferences: string[];
+
+  @Field()
+  @CreateDateColumn({
+    type: 'timestamp with time zone',
+  })
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn({
+    type: 'timestamp with time zone',
+  })
+  updatedAt: Date;
+}
+
+@ObjectType()
+export class GetInvestorType {
+  @Field(() => [Investor], { nullable: true })
+  data?: Investor[];
+
+  @Field(() => Number, { nullable: true })
+  count?: number;
+}
