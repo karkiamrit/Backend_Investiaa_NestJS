6a90f4b1002b58519464b44f065745d118ac5cee
karkiamrit <amrit.karki2073@gmail.com>
2023-11-07 19:56:06 +0545

    created investor repository

---
+++ src/investor/investor.repository.ts
@@ -0,0 +1,11 @@
+import { Investor } from './entities/investor.entity';
+import { CustomRepository } from '../modules/decorators/typeorm.decorator';
+import { Repository } from 'typeorm/repository/Repository';
+
+@CustomRepository(Investor)
+export class InvestorRepository extends Repository<Investor> {
+
+    async findInvestorByUserId(userId: number): Promise<Investor | null> {
+        return await Investor.findOne({ where: { user: { id: userId } } });
+      }
+}
