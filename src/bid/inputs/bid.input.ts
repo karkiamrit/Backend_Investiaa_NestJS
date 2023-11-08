76a449797ff76997d0a53ebbb3b3e60e66960c10
karkiamrit <amrit.karki2073@gmail.com>
2023-11-08 20:28:42 +0545

    bid input made

---
+++ src/bid/inputs/bid.input.ts
@@ -0,0 +1,58 @@
+import { IsArray, IsNotEmpty, IsOptional } from 'class-validator';
+import { Field, InputType } from '@nestjs/graphql';
+
+@InputType()
+export class CreateBidInput {
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  loan_amount?: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  loan_percent?: number;
+
+  @Field(() => Number)
+  @IsNotEmpty()
+  equity: number;
+
+  @Field(() => Number)
+  @IsNotEmpty()
+  loan_years: number;
+
+  @Field(() => Number)
+  @IsNotEmpty()
+  equity_amount: number;
+
+  @Field(() => [String])
+  @IsArray()
+  @IsNotEmpty()
+  service_offered: string[];
+}
+
+@InputType()
+export class UpdateBidInput {
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  loan_amount?: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  loan_percent?: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  equity: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  loan_years: number;
+
+  @Field(() => Number, { nullable: true })
+  @IsOptional()
+  equity_amount: number;
+
+  @Field(() => [String], { nullable: true })
+  @IsArray()
+  @IsOptional()
+  service_offered: string[];
+}
