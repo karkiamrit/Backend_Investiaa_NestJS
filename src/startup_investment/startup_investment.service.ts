3be674899b7ff46b642c34477146d5a8c798f4d8
karkiamrit <amrit.karki2073@gmail.com>
2023-11-21 13:41:19 +0545

    startup investment service added

--- src/startup_investment/startup_investment.service.ts
+++ src/startup_investment/startup_investment.service.ts
@@ -1,54 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { OneRepoQuery, RepoQuery } from 'src/declare/types';
-import { Startup_investmentRepository } from './startup_investment.repository';
-import { Startup_investment } from './entities/startup_investment.entity';
-import {
-  CreateStartup_investmentInput,
-  UpdateStartup_investmentInput,
-} from './inputs/startup_investment.input';
+import { StartupInvestmentRepository } from './startup_investment.repository';
+import { StartupInvestment } from './entities/startup_investment.entity';
+import { Bid } from 'src/bid/entities/bid.entity';
+import { Entrepreneur } from 'src/entrepreneur/entities/entrepreneur.entity';
 
 @Injectable()
-export class Startup_investmentService {
+export class StartupInvestmentService {
   constructor(
-    private readonly startup_investmentRepository: Startup_investmentRepository,
+    private readonly startupInvestmentRepository: StartupInvestmentRepository,
   ) {}
 
-  getMany(qs?: RepoQuery<Startup_investment>, query?: string) {
-    return this.startup_investmentRepository.getMany(qs || {}, query);
+  getOne(qs: OneRepoQuery<StartupInvestment>, query?: string) {
+    return this.startupInvestmentRepository.getOne(qs, query);
   }
 
-  getOne(qs: OneRepoQuery<Startup_investment>, query?: string) {
-    return this.startup_investmentRepository.getOne(qs, query);
-  }
-
-  create(input: CreateStartup_investmentInput): Promise<Startup_investment> {
-    return this.startup_investmentRepository.save(input);
-  }
-
-  createMany(
-    input: CreateStartup_investmentInput[],
-  ): Promise<Startup_investment[]> {
-    return this.startup_investmentRepository.save(input);
-  }
-
-  async update(
-    id: number,
-    input: UpdateStartup_investmentInput,
-  ): Promise<Startup_investment> {
-    const startup_investment = await this.startup_investmentRepository.findOne({
-      where: { id },
-    });
-    return this.startup_investmentRepository.save({
-      ...startup_investment,
-      ...input,
-    });
-  }
-
-  async delete(id: number) {
-    const startup_investment = this.startup_investmentRepository.findOne({
-      where: { id },
-    });
-    await this.startup_investmentRepository.delete({ id });
-    return startup_investment;
+  async create(
+    bid: Bid,
+    entrepreneur: Entrepreneur,
+  ): Promise<StartupInvestment> {
+    const startupInvestment = new StartupInvestment();
+    startupInvestment.bid = bid;
+    startupInvestment.entrepreneur = entrepreneur;
+    return await this.startupInvestmentRepository.save(startupInvestment);
   }
 }
